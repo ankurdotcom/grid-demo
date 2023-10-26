d9359c827672036b6c682c38908a4b13ebd042dd
Ankur Gupta <ankurdotcom@gmail.com>
2023-10-27 00:12:37 +0530

    fixed error

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,18 @@
 const Router = require ('express');
-
 const router = Router();
+const path = require('path');
+
 
 
 router.get("/", (req, res) => res.json({message : "Hello This is grid Demo"}))
+
+router.get("/grid", (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.sendFile(path.join(path.join(__dirname+'/../pages/index.html')));
+})
 
 router.get("/health", (req, res) => 
     res.status(200).json({message: "Everythid is good"})
 )
 
-export default router;
+module.exports = router
