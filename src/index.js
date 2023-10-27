622dbd705d9cac367d0170b1e22b26629194976e
Ankur Gupta <ankurdotcom@gmail.com>
2023-10-27 17:22:32 +0530

    Initial App Structure

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@
 
 
 const express = require('express');
-const router = require('./routes/router')
+const router = require('./routes/router');
+const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT ?? 8000
-
+const PORT = process.env.PORT || 8000
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use("/", router);
+
+// app.use(express.static('public'))
+
+// app.use(express.static('style')) # Multiple Fonders Can be Made Public
+// app.use(express.static('js')) // client Side JS files
 
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
--- src/routes/router.js
+++ src/routes/router.js
@@ -8,7 +8,26 @@ router.get("/hello", (req, res) => res.json({message : "Hello This is grid Demo"
 
 router.get("/", (req, res) => {
     res.set('Content-Type', 'text/html');
-    res.sendFile(path.join(path.join(__dirname+'/../pages/index.html')));
+    res.sendFile(path.join(__dirname+'/../pages/home.html'));
+})
+
+router.get("/home", (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.sendFile(path.join(__dirname+'/../pages/home.html'));
+})
+
+router.get("/newEntry", (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.sendFile(path.join(path.join(__dirname+'/../pages/newEntryForm.html')));
+})
+
+router.get("/newEntrySave", (req, res) => {
+    res.status(200).end("Saved Successfully");
+})
+
+router.get("/reports", (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.sendFile(path.join(path.join(__dirname+'/../pages/reports.html')));
 })
 
 router.get("/health", (req, res) => 
